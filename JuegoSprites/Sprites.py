1942c855197652fd32b9214606ff50679687126b
A01375925 <Minecraft3199>
2017-11-14 09:18:51 -0600

    Sprites

--- JuegoSprites/Sprites.py
+++ JuegoSprites/Sprites.py
@@ -2,10 +2,10 @@
 #Javier Pascal Flores
 
 import pygame
-
+from random import randint
 # Dimensiones de la pantalla
 ancho = 800
-alto = 800
+alto = 900
 # Colores
 blanco = (255,255,255)  # R,G,B en el rango [0,255]
 verde_bandera = (0, 122, 0)
@@ -23,13 +23,50 @@ def drawGame(ventana, enemiesList, bulletList):
     for bullet in bulletList:
         ventana.blit(bullet.image, bullet.rect)
 
-def actualizarBalas(bulletlist, enemiesList):
-    for bullet in bulletlist: #BO NOT MODIFY CONNECTION
+def refreshBullets(bulletlist, enemiesList):
+    for bullet in bulletlist: #DO NOT MODIFY CONNECTION
         bullet.rect.top -= 15
+        if bullet.rect.top < -15:
+            bulletlist.remove(bullet)
+            continue
+        delBullet= False
         for k in range(len(enemiesList)-1,-1,-1):
             enemy= enemiesList[k]
             if bullet.rect.colliderect(enemy):
                 enemiesList.remove(enemy)
+                delBullet= True
+                break
+        if delBullet == True:
+            bulletlist.remove(bullet)
+
+
+def spawnEnemies(enemiesList, imgEnemy):
+    for x in range(5):
+        for y in range(4):
+            #X,Y coordenates
+            cx = 50  + x*150
+            cy = 50 + y*150
+            enemy = pygame.sprite.Sprite()
+            enemy.image = imgEnemy
+            enemy.rect = imgEnemy.get_rect()
+            enemy.rect.left = cx - enemy.rect.width//2
+            enemy.rect.top = cy - enemy.rect.height//2
+            enemiesList.append(enemy)
+    for enemy in enemiesList:
+        enemy.rect.top -= 15
+
+
+
+def randomEnemies(enemiesList, imgEnemy):
+
+    enemy = pygame.sprite.Sprite()
+    enemy.image = imgEnemy
+    enemy.rect = imgEnemy.get_rect()
+    cx = randint(10, ancho-enemy.rect.width)
+    cy = randint(10, alto-enemy.rect.top)
+    enemy.rect.left = cx
+    enemy.rect.top = cy
+    enemiesList.append(enemy)
 
 def dibujar():
     # Ejemplo del uso de pygame
@@ -51,11 +88,14 @@ def dibujar():
     #Enemies
     enemiesList=[]
     imgEnemy = pygame.image.load("ALien.png")
+    spawnEnemies(enemiesList, imgEnemy)
 
     #Balas
     bulletList=[]
     imgBullet = pygame.image.load("Bullet.png")
 
+    timer = 0
+    puntos = 0
 
     while not termina:
         # Procesa los eventos que recibe
@@ -84,18 +124,31 @@ def dibujar():
                     bullet.image =imgBullet
                     bullet.rect = imgBullet.get_rect()
                     bullet.rect.left = xm
-                    bullet.rect.top = alto - bullet.rect.height
+                    bullet.rect.top = alto + bullet.rect.height
                     bulletList.append(bullet)
 
 
         # Borrar pantalla
         ventana.fill(negro)
+        puntos+=.2
+        font = pygame.font.SysFont("monospace", 18)
+        text = font.render("Puntos:" + str(round(puntos,3)), 1, blanco)
+        ventana.blit(text, (10, 10))
+
+        #Enemies every 2 sec
+
+        timer += 1/40
+        if timer >=2:
+            timer = 0
+            randomEnemies(enemiesList, imgEnemy)
+
 
         # Dibujar, aquí haces todos los trazos que requieras
         if state == "menu":
             drawMenu(ventana, playButton)
         elif state == "jugando":
-            actualizarBalas(bulletList, enemiesList)
+
+            refreshBullets(bulletList, enemiesList)
             drawGame(ventana, enemiesList, bulletList)
 
 
