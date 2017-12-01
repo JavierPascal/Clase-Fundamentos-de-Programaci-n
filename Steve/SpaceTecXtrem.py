060973f2e78eafe5d6bc54a3a9077cebd4483506
Javier Pascal Flores <A01375925@itesm.mx>
2017-12-01 01:46:45 -0600

    Videojuego Listo PAra entregar

--- Steve/SpaceTecXtrem.py
+++ Steve/SpaceTecXtrem.py
@@ -3,23 +3,28 @@
 
 import pygame
 from random import randint
+from collections import OrderedDict
 
 # Dimensiones de la pantalla
 ancho = 800
 alto = 600
+screen = pygame.display.set_mode((ancho,alto))
 # Colores
 blanco = (255, 255, 255)  # R,G,B en el rango [0,255]
 verde_bandera = (0, 122, 0)
 rojo = (255, 0, 0)
 negro = (0, 0, 0)
-
+print("Moverse = Mouse" + "\n" + "Disparar = Espacio")
+username=input("Dame tu username: ")
 
 
 def drawMenu(ventana, playButton, Logo, high):
     ventana.blit(playButton.image, playButton.rect)
     ventana.blit(Logo.image, Logo.rect)
     ventana.blit(high.image, high.rect)
-
+def drawGAmeover(ventana):
+    imgGO=pygame.image.load("Images/Gameover.png")
+    ventana.blit(imgGO,(200,200))
 
 
 def drawGame(ventana, enemiesList, bulletList):
@@ -108,22 +113,79 @@ def randomEnemies(enemiesList, imgEnemy, imgEnemy_69,img_Comet):
     enemiesList.append((comet))
 
 
-def drawHighscores(ventana, Logo):
+def drawHighscores(ventana, Logo, screen,state):
+    entrada = open("highscores.txt", "r", encoding='UTF-8')
+    contenido = entrada.readlines()
+
+    entrada.close()
+
     imgSpace = pygame.image.load("images/Back/Space.png")
     ventana.blit(imgSpace,(0,0))
     ventana.blit(Logo.image, Logo.rect)
-    open("highscore.txt", "w", encoding="UTF-8")
+    displacement=0
+    for linea in contenido:
+
+        displacement+=30
+        font = pygame.font.SysFont("monospace", 25)
+        text = font.render(str(linea), 5, rojo)
+        textrect = text.get_rect()
+        textrect.centerx = screen.get_rect().centerx
+        textrect.centery = screen.get_rect().centery+displacement
+        ventana.blit(text, textrect)
+    xm, ym = pygame.mouse.get_pos()
+    imgButPlay = pygame.image.load("Images/button_jugar.png")
+    # Sprite
+    playButton = pygame.sprite.Sprite()
+    playButton.image = imgButPlay
+    playButton.rect = imgButPlay.get_rect()
+    playButton.rect.left = 0
+    playButton.rect.top = 0
+    xb, yb, anchoB, altoB = playButton.rect
+    if xm >= xb and xm <= xb + anchoB:
+        if ym >= yb and ym <= yb + altoB:
+            state= "jugando"
+    ventana.blit(playButton.image, playButton.rect)
+    return state
+
+
+
+def highscoresinterpreter(highscoresDicc,usuario, score):
+    newFile = open("highscores.txt","w",encoding='UTF-8')
+    highscoresDicc[usuario]=score
+    orderedDict = dict(OrderedDict(sorted(highscoresDicc.items(), key=lambda t: t[1], reverse=False)))
+    keys = []
+    values = []
+    for key in highscoresDicc:
+        keys.append(key)
+        values.append(highscoresDicc[key])
+    print(keys,values)
+    newFile.write("0,Username,Play Time,n\n")
+    for index in range(len(orderedDict)):
+        newFile.write(str(index+1) + ","+ keys[index] +","+ str(values[index]) +",n\n")
+    newFile.close()
+
+def makeHighscores():
+    highscoresDicc = {}
+    file = open("highscores.txt", "r", encoding="UTF-8")
+    content = file.readlines()
+    file.close()
+
+    content.remove(content[0])
+
+    for line in content:
+        newLine = line.split(",")
+        highscoresDicc[newLine[1]] = float(newLine[2])
+    return highscoresDicc
 
 
 def Livesubstract(ventana, bogo, enemiesList, lives):
-
-
     for enemy in enemiesList:
         if pygame.sprite.collide_rect(bogo, enemy):
             lives -= 1
     font = pygame.font.SysFont("monospace", 18)
-    text = font.render("Vidas:" + str(round(lives, 3)), 1, blanco)
+    text = font.render("Vidas:" + str(lives), 1, blanco)
     ventana.blit(text, (10, 10))
+    return lives
 
 def dibujar():
     # Ejemplo del uso de pygame
@@ -132,7 +194,7 @@ def dibujar():
     reloj = pygame.time.Clock()  # Para limitar los fps
     termina = False  # Bandera para saber si termina la ejecución
     state = "menu"  # playing, end
-    lives=3
+    lives=1
 
     # Load images
     imgButPlay = pygame.image.load("Images/button_jugar.png")
@@ -166,6 +228,7 @@ def dibujar():
 
     #Fondo de Menu
     imgSpace = pygame.image.load("images/Back/Space.png")
+    #Game Over
 
 
 
@@ -188,7 +251,13 @@ def dibujar():
 
 
     # Music
+    pygame.mixer.init()
+    pygame.mixer.music.load("Sounds/zizibum.mp3")
+    pygame.mixer.music.play(-1)
 
+
+    highscoreDicc=makeHighscores()
+    score=0
 
     while not termina:
         # Procesa los eventos que recibe
@@ -220,6 +289,7 @@ def dibujar():
 
             elif evento.type == pygame.KEYDOWN:
                 if evento.key == pygame.K_SPACE:
+
                     bullet = pygame.sprite.Sprite()
                     bullet.image = imgBullet
                     bullet.rect = imgBullet.get_rect()
@@ -236,7 +306,7 @@ def dibujar():
         # Enemies every 2 sec
 
         timer += 1 / 40
-        if timer >= 2:
+        if timer >= 1:
             timer = 0
             randomEnemies(enemiesList, imgEnemy, imgEnemy_69, img_Comet)
 
@@ -246,7 +316,12 @@ def dibujar():
             drawMenu(ventana, playButton,Logo,highButton)
         elif state == "highscore":
 
-            drawHighscores(ventana, Logo)
+            estado=drawHighscores(ventana, Logo, screen, state)
+            if estado =="jugando":
+                state="jugando"
+
+
+
 
 
         elif state == "jugando":
@@ -254,6 +329,7 @@ def dibujar():
 
             ventana.blit(image_back, (0, y))
             y -= 9
+
             if y <= -10000:
                 y = 0
 
@@ -261,13 +337,18 @@ def dibujar():
             refreshEnemies(enemiesList)
             drawGame(ventana, enemiesList, bulletList)
             bogo=DibujarBogo(ventana)
-            Livesubstract(ventana, bogo, enemiesList, lives)
+            lives=Livesubstract(ventana, bogo, enemiesList, lives)
+            if lives ==0:
+                state ="gameover"
+        elif state == "gameover":
+            drawGAmeover(ventana)
 
-
+        score+=1
         pygame.display.flip()  # Actualiza trazos
         reloj.tick(40)  # 40 fps
 
     pygame.quit()  # termina pygame
+    highscoresinterpreter(highscoreDicc, username, score)
 
 
 def main():
